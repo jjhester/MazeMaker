6781c906e61304236eb3e417b93a8144b5032fc7
jhester <jjhester6@gmail.com>
2015-05-17 17:55:11 -0400

    Completed basic wall implementation
    Ready for the actual maze algorithms this time, I promise!

--- Assets/Scripts/Components/Perimeter.cs
+++ Assets/Scripts/Components/Perimeter.cs
@@ -13,7 +13,40 @@ public class Perimeter : MonoBehaviour {
    private bool[] walls = new bool[4];
    public bool this [Direction wallDirection] {
 	 get { return walls [(int)wallDirection];}
-	 set { walls [(int)wallDirection] = value;}
+	 set {
+	    if (value != walls [(int)wallDirection]) {
+		  walls [(int)wallDirection] = value;
+		  if (value) {
+			AddWall(wallDirection);
+		  } else {
+			RemoveWall(wallDirection);
+		  }
+	    }
+	 }
+   }
+   private void AddWall ( Direction wallDirection ) {
+	 float yRotate = ((int)wallDirection % 2 == 0)? 0: 90;
+	 float z = (float)((yRotate > 0)? 0: ((int)wallDirection > 0)? -.5 : .5);
+	 float x = (float)((yRotate > 0)? ((int)wallDirection > 1)? -.5 : .5 : 0);
+
+	 Vector3 parentPosition = this.gameObject.transform.position;
+	 Vector3 position = new Vector3(parentPosition.x + x, parentPosition.y, parentPosition.z + z);
+	 Vector3 rotation = new Vector3(0f,yRotate,0f);
+	 GameObject curWall = Instantiate(wallObject, position, Quaternion.identity) as GameObject;
+	 curWall.transform.Rotate(rotation);
+	 curWall.gameObject.transform.parent = this.gameObject.transform;
+	 curWall.gameObject.name = string.Format("Wall{0}", wallDirection);
+   }
+	private void RemoveWall (Direction wallDirection) {
+		Transform curWall = this.gameObject.transform.Find(string.Format("Wall{0}", wallDirection));
+		GameObject.Destroy(curWall.gameObject);
+	}
+   void Start () {
+//	 //By default has all four walls
+//	this[Direction.NORTH] = true;
+//     this[Direction.EAST] = true;
+//     this[Direction.SOUTH] = true;
+//     this[Direction.WEST] = true;
    }
 }
 
--- Assets/Scripts/Controllers/MazeGenerator.cs
+++ Assets/Scripts/Controllers/MazeGenerator.cs
@@ -11,12 +11,18 @@ public class MazeGenerator : MonoBehaviour {
 	 this.tiles = new GameObject[height, width];
 	 for (byte r = 0; r < height; r++) {
 	    for (byte c = 0; c < width; c++) {
-		  Vector3 position = new Vector3(r, 0, (width - 1 - c));
+		  Vector3 position = new Vector3(c, 0, height - r);
 		  GameObject curTile = Instantiate(tileObject, position, Quaternion.identity) as GameObject;
 		  curTile.gameObject.transform.parent = this.gameObject.transform;
 		  curTile.gameObject.name = string.Format("Tile({0},{1})", r, c);
 		  this.tiles [r, c] = curTile;
 		  Debug.Log(this.tiles [r, c]);
+		  //Handle walls
+		  Perimeter walls = curTile.GetComponent<Perimeter>();
+		  walls [Direction.NORTH] = r == 0;
+		  walls [Direction.SOUTH] = r == height - 1;
+		  walls [Direction.WEST] = c == 0;
+		  walls [Direction.EAST] = c == width - 1;
 	    }
 	 }
    }
